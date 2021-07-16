b93d9214133f595825606a937fc699aa07e3be5b
Zainab Eid <zainabeid2012@gmail.com>
2021-07-16 23:15:46 +0200

    edit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ClinicsTableSeeder::class);
         $this->call(ClinicsCategoriesTableSeeder::class);
+        $this->call(ClinicsTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(AdminTableSeeder::class);
