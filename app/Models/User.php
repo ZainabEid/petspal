e0cd2485529d77db4adf6f10e50b7bae6179aefb
Zainab Eid <zainabeid2012@gmail.com>
2021-07-25 17:40:06 +0200

    s

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
 {
     use SoftDeletes;
     use HasFactory, Notifiable;
-    use HasApiTokens;
+    // use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
