ae63bdc8e38834c1ea740af96cfb48ce038687b5
Zainab Eid <zainabeid2012@gmail.com>
2021-08-11 16:30:47 +0200

    dashboard page

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -23,6 +23,7 @@ class DashboardController extends Controller
     public function __call($method,$arameters)
     {
         $page = Page::where('page->en' ,$method)->first();
+        dd($page->page);
         return view('admin.pages.page',compact('method','page'));
     }
 
